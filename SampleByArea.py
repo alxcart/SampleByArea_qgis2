2b088fcbabbf7131069c0a00c2917b1439b9df44
Alex <alxcart@gmail.com>
2018-12-10 17:59:22 -0200

    Update SampleByArea.py
    Alteração do grupo para GCQ - Plano de amostragem
    Melhorias de entendimento do script

--- SampleByArea.py
+++ SampleByArea.py
@@ -17,23 +17,22 @@ email                : alex.santos@ibge.gov.br / viviane.diniz@ibge.gov.br
  *                                                                         *
  ***************************************************************************/
  /***************************************************************************
- *   Manual de uso:                                                        * 
+ *   Manual de uso:                                                        *
  *   Este script foi elaborado para ser utilizado no ambiente SIG QGIS.    *
  *   Desta forma, no ambiente QGIS, realize os seguintes procedimentos de  *
  *   instalação:                                                           *
- *        1 - Em “Processar” habilitar a “Caixa de Ferramentas”.           * 
- *        2 - Em “Scripts” ir até o menu “Tools” > “Add script from file”. *  
- *   Adicionar o script ".py" desejado.                                    *
- *  Nota:                                                                  *  
+ *        1 - Em “Processar” habilitar a “Caixa de Ferramentas”.           *
+ *        2 - Em “Scripts” ir até o menu “Tools” > “Add script from file”. *
+ *            Adicionar o script ".py" desejado.                           *
+ *  Nota:                                                                  *
  *   as versões mais recentes e novos desenvolvimentos ficaram disponíveis *
  *   em: https://github.com/alxcart                                        *
  ***************************************************************************/
  Fonte: Adaptado de:
  Grid generation code adapted from https://gis.stackexchange.com/questions/79916/how-to-generate-a-grid-programmatically-in-python-without-gui
  """
-##Controle de Qualidade=group
-##Gera Areas para Inspecao=name
-
+##GCQ - Plano de amostragem=group
+##2-Inspecao por area=name
 ##Camada=vector
 ##Tamanho_da_celula_da_grade=number 0.0
 ##Nivel_de_Inspecao=selection I;II;III
@@ -47,7 +46,6 @@ from qgis.utils import iface
 from math import ceil
 import random
 from qgis.gui import QgsMessageBar
-
 
 lyrInput = processing.getObject( Camada )
 xmin,ymin,xmax,ymax = lyrInput.extent().toRectF().getCoords()
@@ -73,7 +71,7 @@ for i in range(int(cols)):
     ringYtop = ringYtopOrigin
     ringYbottom =ringYbottomOrigin
     for j in range(int(rows)):
-        points = [QgsCore.QgsPoint(ringXleftOrigin, ringYtop),QgsCore.QgsPoint(ringXrightOrigin, ringYtop),QgsCore.QgsPoint(ringXrightOrigin, ringYbottom),QgsCore.QgsPoint(ringXleftOrigin, ringYbottom),QgsCore.QgsPoint(ringXleftOrigin, ringYtop)] 
+        points = [QgsCore.QgsPoint(ringXleftOrigin, ringYtop),QgsCore.QgsPoint(ringXrightOrigin, ringYtop),QgsCore.QgsPoint(ringXrightOrigin, ringYbottom),QgsCore.QgsPoint(ringXleftOrigin, ringYbottom),QgsCore.QgsPoint(ringXleftOrigin, ringYtop)]
         request = QgsCore.QgsFeatureRequest(QgsCore.QgsRectangle(ringXleftOrigin,ringYtop,ringXrightOrigin,ringYbottom))
         for feature in  lyrInput.getFeatures(request):
             square = QgsCore.QgsFeature()
@@ -89,9 +87,8 @@ for i in range(int(cols)):
         ringYbottom = ringYbottom - gridHeight
     ringXleftOrigin = ringXleftOrigin + gridWidth
     ringXrightOrigin = ringXrightOrigin + gridWidth
-    
-progress.setInfo( 'Total de elementos da grade: ' + str(lyrIntermediate.featureCount()))
 
+progress.setInfo( 'Total de elementos da grade: ' + str(lyrIntermediate.featureCount()))
 
 dicSampleLength={2:[2,2,3],9:[2,3,5],16:[3,5,8],26:[5,8,13],51:[5,13,20],91:[8,20,32],151:[13,32,50],281:[20,50,80],501:[32,80,125],1201:[50,125,200],3201:[80,200,315],10001:[125,315,500],35001:[200,500,800],150001:[315,800,1250],500001:[500,1250,2000]}
 
@@ -109,23 +106,17 @@ step= lyrIntermediate.featureCount() // dicSampleLength[index1][Nivel_de_Inspeca
 progress.setInfo( 'Passo: ' + str(step))
 progress.setInfo( 'Sorteado: ' + str(randomNum))
 module=randomNum % step
-listIds=range(lyrIntermediate.featureCount()) 
+listIds=range(lyrIntermediate.featureCount())
 notSelected=[]
 
 progress.setInfo('Selecionando celulas da amostra...')
 for i in listIds:
     if not (i + 1) % step == module:
         notSelected.append(i + 1);
-   
+
 lyrIntermediate.dataProvider().deleteFeatures(notSelected)
 
 for f in lyrIntermediate.getFeatures():
     lyrOutput.addFeature(f)
-    
+
 QMessageBox.about(None, "Amostragem aleatoria sistematica", "Unidades de amostragem criadas")
-
-
-
-
-
-
